11bce989b3cb2a45a1cedfd1720964c7272eabab
Tanmay Singh <tanmaysingh807@gmail.com>
2025-09-05 12:29:51 +0530

    im new to git my bad gng
    \Changes to be committed:
    	new file:   stringreturn.class
    	new file:   stringreturn.java
    
     Untracked files:
    	../dsa.kunalkushwaha/

---
+++ methods/stringreturn.java
@@ -0,0 +1,12 @@
+public class stringreturn {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet() {
+        String greeting = "how are you lilnigga";
+        return greeting;
+
+    }
+}
