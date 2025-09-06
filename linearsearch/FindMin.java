e01452ebee70530d993f50c7028661bef4fcccc3
Tanmay Singh <tanmaysingh807@gmail.com>
2025-09-07 00:57:55 +0530

    2d array searching and maxval

--- linearsearch/FindMin.java
+++ linearsearch/FindMin.java
@@ -1,17 +1,17 @@
 public class FindMin {
     public static void main(String[] args) {
         int[] arr = {18, 12, 7, 3, 14, 28};
-        System.out.println(min(arr));
+        System.out.println(max(arr));
     }
     // assume arr.length != 0
     // return the minimum value in the array
-    static int min(int[] arr) {
+    static int max(int[] arr) {
         int ans = arr[0]; //initiliases ans as the first element of the array
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < ans) {
+            if (arr[i] > ans) {
                 ans = arr[i];
             }
         }
         return ans;
     }
-}
+} 
